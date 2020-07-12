220a5154a7348872c8c8b715d574513f0d289188
Imam Digmi <imam.digmi@gmail.com>
2020-07-12 11:32:54 +0700

    Save to github

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chat_ui/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        accentColor: Color(0xFFFEF9EB),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
